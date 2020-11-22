7332a23068e115f6bd06aaf295c01d9fc28ac545
Skeletonek <skeleton0199@gmail.com>
2020-11-22 21:43:25 +0100

    Add files via upload

---
+++ 22_11_2020_Petle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            for (; i < 5; i++)
+            {
+                Console.Write("Test!");
+            }
+            Console.WriteLine();
+            while (i < 5)
+            {
+                Console.Write("Test!");
+                i++;
+            }
+            Console.WriteLine();
+            do
+            {
+                Console.Write("Test!");
+                k++;
+            } while (i < 5);
+        }
+    }
+}
