d64e9660f24ce15a179d9884b2e21ba91a90585d
Skeletonek <skeleton0199@gmail.com>
2020-11-27 14:35:49 +0100

    WO_Petle, WO_TryCatch

---
+++ Wiedza Ogólna/WO_Petle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Private_Lessons
+{
+    class WO_Petle
+    {
+        static void Main(string[] args)
+        {
+            // PĘTLA FOR
+
+            // Budowa: 
+            // for(<deklaracja zmiennej>; <warunek działania pętli>; <czynność na zakończenie jednej iteracji pętli>)
+            // {
+            // <zawartość pętli>
+            // }
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Została wykonana jedna iteracja pętli for");
+            }
+
+            Console.WriteLine();
+            // Pętla for nie musi posiadać wypełnionych wszystkich argumentów
+
+            int forLoop = 0;
+            for(;forLoop < 5; forLoop++)
+            {
+                Console.WriteLine("Została wykonana jedna iteracja pętli for z wykorzystaniem zmiennej forloop");
+            }
+
+            Console.WriteLine();
+            // W ten sposób możemy stworzyć pętle która będzie wykonywana w nieskończoność
+
+            // for(;;)
+            // {
+            // <zawartość pętli>
+            // }
+
+            // PĘTLA WHILE i DO...WHILE
+
+            int whileLoop = 0;
+            int doWhileLoop = 0;
+
+            while(whileLoop < 5) // Definicja warunku na początku
+            {
+                Console.WriteLine("Została wykonana jedna iteracja pętli while");
+                whileLoop++; // Wymagane jest ręczne wpisanie zmiany wartości zmiennej używanej w warunku. Inaczej pętla będzie wykonywana w nieskończoność.
+            }
+
+            do
+            {
+                Console.WriteLine("Została wykonana jedna iteracja pętli do...while"); 
+                doWhileLoop++;
+            } while (doWhileLoop < 5); // Definicja warunku na końcu z średnikiem na zakończenie
+
+            Console.WriteLine();
+            // Różnica między tymi pętlami jest taka, że pętla do...while zawsze wykona się przynajmniej raz niezależnie od warunku.
+            // Pętla while wykona się tylko jeśli warunek jest spełniony
+
+            // Ponieważ nasze zmienne whileLoop i doWhileLoop mają już wartość 5, czyli nie spełniają warunku, że 5 < 5 możemy to zaobserwować jeszcze raz uruchamiając pętle
+
+            while (whileLoop < 5)
+            {
+                Console.WriteLine("Została wykonana jedna iteracja pętli while, w momencie gdy whileLoop już ma wartość 5");
+                whileLoop++;
+            }
+
+            do
+            {
+                Console.WriteLine("Została wykonana jedna iteracja pętli do...while, w momencie gdy doWhileLoop już ma wartość 5");
+                whileLoop++;
+            } while (doWhileLoop < 5);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Wiedza Ogólna/WO_TryCatch.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Private_Lessons
+{
+    class WO_TryCatch
+    {
+        static void Main(string[] args)
+        {
+            string zmiennaNiszczącaProgram = "Zniszczę twoje życie";
+            int niczegoNiewinnaZmienna;
+
+            // niczegoNiewinnaZmienna = int.Parse(zmiennaNiszczącaProgram);
+            // Ta instrukcja była zakomentowana, ponieważ spowodowałaby wyjątek (System.FormatException), który "zcrashowałby" nasz program
+            // Oczywiście jest tak dlatego, że program nie jest w stanie zrozumieć zdania "Zniszczę twoje życie" jako liczby
+
+            // Możemy to łatwo ominąć na dwa sposoby
+
+            try
+            {
+                niczegoNiewinnaZmienna = int.Parse(zmiennaNiszczącaProgram);
+            }
+            catch
+            {
+                Console.WriteLine($"Nie potrafię zrozumieć \"{zmiennaNiszczącaProgram}\" jako liczby!");
+                Console.WriteLine($"Ustawiam wartość zmiennej niczegoNiewinnaZmienna na 0");
+                niczegoNiewinnaZmienna = 0;
+            }
+
+            Console.WriteLine($"Wartość zmiennej niczegoNiewinnaZmienna = {niczegoNiewinnaZmienna}\n");
+
+            // Możemy dokładnie doprecyzować co ma się wykonać w przypadku jakiego wyjątku
+
+            try
+            {
+                niczegoNiewinnaZmienna = int.Parse(zmiennaNiszczącaProgram);
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine($"Nie potrafię zrozumieć \"{zmiennaNiszczącaProgram}\" jako liczby! Wykryto wyjątek: \"System.FormatException\"");
+                Console.WriteLine($"Ustawiam wartość zmiennej niczegoNiewinnaZmienna na 0");
+                niczegoNiewinnaZmienna = 0;
+            }
+
+            Console.WriteLine($"Wartość zmiennej niczegoNiewinnaZmienna = {niczegoNiewinnaZmienna}\n");
+
+            // Try, Catch będziemy używali ogólnie dla wszystkich przypadków gdzie może nastąpić wyjątek.
+            // Oczywiście aby Try, Catch zostało użyte, programista musi przewidzieć że taki błąd może wystąpić, lub wykryć wyjątek na etapie testów aplikacji
+
+            // W przypadku konwersji tekstu na liczby możemy użyć też specjalnej instrukcji TryParse
+
+            Console.WriteLine($"Wartość dla metody TryParse: {zmiennaNiszczącaProgram}");
+            int.TryParse(zmiennaNiszczącaProgram, out niczegoNiewinnaZmienna); // Jeżeli instrukcji TryParse nie uda się przekonwertować znaków, ustawi ona wartość zmiennej na 0
+            Console.WriteLine($"Wartość zmiennej niczegoNiewinnaZmienna = {niczegoNiewinnaZmienna}\n");
+
+            Console.WriteLine($"Wartość dla metody TryParse: 30");
+            int.TryParse("30", out niczegoNiewinnaZmienna); //Jeśli się jednak uda, to wiadomo jaka będzie wartość :-)
+            Console.WriteLine($"Wartość zmiennej niczegoNiewinnaZmienna = {niczegoNiewinnaZmienna}\n");
+
+            // TryParse jest metodą, która może zwracać dodatkową wartość. Jest to wartość typu bool, która informuje nas czy konwersja się udała
+
+            if(int.TryParse(zmiennaNiszczącaProgram, out niczegoNiewinnaZmienna)) // Sprawdzenie czy funkcja zwrociła wartość true
+            {
+                Console.WriteLine($"Udało się przekonwertować liczbę!");
+            }
+            else
+            {
+                Console.WriteLine($"Nie udało się przekonwertować liczby!");
+            }
+            Console.WriteLine($"Wartość zmiennej niczegoNiewinnaZmienna = {niczegoNiewinnaZmienna}\n");
+
+            // I w przypadku udanej konwersji...
+
+            if (int.TryParse("66", out niczegoNiewinnaZmienna))
+            {
+                Console.WriteLine($"Udało się przekonwertować liczbę!");
+            }
+            else
+            {
+                Console.WriteLine($"Nie udało się przekonwertować liczby!");
+            }
+            
+            Console.WriteLine($"Wartość zmiennej niczegoNiewinnaZmienna = {niczegoNiewinnaZmienna}\n");
+
+            Console.ReadKey();
+        }
+    }
+}
